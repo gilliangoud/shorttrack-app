44f1b0b98d2f5020b1499d43c03e6da7bef156a3
Gillian Goud <gilliangoud@hotmail.com>
2022-11-19 13:08:52 +0100

    tested out supabase auth and RLS

---
+++ src/pages/login.tsx
@@ -0,0 +1,40 @@
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useEffect, useState } from 'react'
+import { type Database } from '../types/database.types'
+
+const LoginPage = () => {
+  const supabaseClient = useSupabaseClient<Database>()
+  const user = useUser()
+  const [data, setData] = useState<unknown>()
+
+  useEffect(() => {
+    async function loadData() {
+      const { data } = await supabaseClient.from('test').select('*')
+      setData(data)
+    }
+    // Only run query once user is logged in.
+    if (user) loadData()
+  }, [user])
+
+  if (!user)
+    return (
+      <Auth
+        redirectTo="http://localhost:3000/"
+        appearance={{ theme: ThemeSupa }}
+        supabaseClient={supabaseClient}
+      />
+    )
+
+  return (
+    <>
+      <button onClick={() => supabaseClient.auth.signOut()}>Sign out</button>
+      <p>user:</p>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <p>client-side data fetching with RLS</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  )
+}
+
+export default LoginPage
--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -9,30 +9,21 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
-      profiles: {
+      test: {
         Row: {
-          id: string
-          updated_at: string | null
-          username: string | null
-          full_name: string | null
-          avatar_url: string | null
-          website: string | null
+          id: number
+          created_at: string | null
+          title: string | null
         }
         Insert: {
-          id: string
-          updated_at?: string | null
-          username?: string | null
-          full_name?: string | null
-          avatar_url?: string | null
-          website?: string | null
+          id?: number
+          created_at?: string | null
+          title?: string | null
         }
         Update: {
-          id?: string
-          updated_at?: string | null
-          username?: string | null
-          full_name?: string | null
-          avatar_url?: string | null
-          website?: string | null
+          id?: number
+          created_at?: string | null
+          title?: string | null
         }
       }
     }
@@ -40,11 +31,13 @@ export interface Database {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      install_available_extensions_and_test: {
+        Args: Record<PropertyKey, never>
+        Returns: boolean
+      }
     }
     Enums: {
       [_ in never]: never
     }
   }
 }
-
