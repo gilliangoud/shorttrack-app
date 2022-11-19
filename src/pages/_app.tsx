f0735230d87f510bd2a91a10d982a6e467d53f99
Gillian Goud <gilliangoud@hotmail.com>
2022-11-19 12:20:38 +0100

    Add supabase auth-helpers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,31 @@
 import { type AppType } from "next/app";
+import { useState } from "react";
+import {
+  SessionContextProvider,
+  type Session,
+} from "@supabase/auth-helpers-react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { type Database } from "../types/database.types";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType<{ initialSession: Session | null }> = ({
+  Component,
+  pageProps: { initialSession, ...pageProps },
+}) => {
+  const [supabaseClient] = useState(() =>
+    createBrowserSupabaseClient<Database>()
+  );
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { type Database } from '../types/database.types'
 import { env } from "../env/client.mjs";
 
-export default createClient(
+export default createClient<Database>(
   env.NEXT_PUBLIC_SUPABASE_URL,
   env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   {
