afbb093723805e632bc21b984e081f8e93d82717
Gillian Goud <gilliangoud@hotmail.com>
2023-01-30 21:21:26 -0800

    remove supabase-client lib

--- libs/supabase-client/src/lib/supabase-client.spec.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseClient } from './supabase-client';
-
-describe('supabaseClient', () => {
-  it('should work', () => {
-    expect(supabaseClient()).toEqual('supabase-client');
-  });
-});
