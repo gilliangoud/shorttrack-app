f10902fd72eb1f1fc9c4accce8e6f68dd3dd7e56
Gillian Goud <gilliangoud@hotmail.com>
2023-02-09 16:50:04 -0800

    Reverted back to astro ssg

--- apps/reporter-website/astro.config.mjs
+++ apps/reporter-website/astro.config.mjs
@@ -9,17 +9,13 @@ import turbolinks from '@astrojs/turbolinks';
 import alpinejs from "@astrojs/alpinejs";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+
 const siteUrl = process.env.VERCEL_ENV === 'production' ? 'https://shorttrack.app/' // 'your.prod.domain.here'
 : 'http://localhost:3000/';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/reporter-website',
-  output: 'server',
   site: siteUrl,
-  // adapter: vercel({
-  //   analytics: true,
-  // }),
   integrations: [react(), svelte(), partytown(), alpinejs(), sitemap(), tailwind(), turbolinks(), image(), mdx()],
-  adapter: vercel()
 });
