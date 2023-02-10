8f2ec4d733e2365277801db763ddac8aa48b6945
Gillian Goud <gilliangoud@hotmail.com>
2023-02-09 16:30:40 -0800

    updated vercel integration

--- apps/reporter-website/astro.config.mjs
+++ apps/reporter-website/astro.config.mjs
@@ -9,22 +9,17 @@ import turbolinks from '@astrojs/turbolinks';
 import alpinejs from "@astrojs/alpinejs";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+const siteUrl = process.env.VERCEL_ENV === 'production' ? 'https://shorttrack.app/' // 'your.prod.domain.here'
+: 'http://localhost:3000/';
 
+// https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/reporter-website',
   output: 'server',
-  adapter: vercel({
-    analytics: true,
-  }),
-  integrations: [
-    react(),
-    svelte(),
-    partytown(),
-    alpinejs(),
-    sitemap(),
-    tailwind(),
-    turbolinks(),
-    image(),
-    mdx()
-  ],
+  site: siteUrl,
+  // adapter: vercel({
+  //   analytics: true,
+  // }),
+  integrations: [react(), svelte(), partytown(), alpinejs(), sitemap(), tailwind(), turbolinks(), image(), mdx()],
+  adapter: vercel()
 });
