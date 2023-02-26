1e3022485beec542bb87e5ccad90be594032e352
Gillian Goud <gilliangoud@hotmail.com>
2023-02-26 12:10:23 -0800

    add nestjs app to create pat to lynx export cli

---
+++ apps/pat-lynx-export/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'pat-lynx-export',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/pat-lynx-export',
+};
