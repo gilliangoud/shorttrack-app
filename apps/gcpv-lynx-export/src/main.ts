e59596176d9dd03a212675c0780cd7ae9bbef452
Gillian Goud <gilliangoud@hotmail.com>
2023-02-26 20:12:58 -0800

    wrap export logic in file-watcher logic

--- apps/gcpv-lynx-export/src/main.ts
+++ apps/gcpv-lynx-export/src/main.ts
@@ -1,5 +1,6 @@
 import { readFileSync, appendFileSync, unlinkSync } from 'fs';
 import MDBReader from 'mdb-reader';
+import watch from 'node-watch';
 
 import { getCompetitionId } from './app/competition';
 import { getCompetitors, getCompetitorsInCompetition } from './app/competitors';
@@ -10,51 +11,61 @@ const eventFile = process.env.EVT_FILE || '/media/LYNX.EVT';
 const mdb = new MDBReader(readFileSync(syncLocation));
 const competition_no = getCompetitionId(mdb);
 
-// delete file if it exists
-try {
-  unlinkSync(eventFile);
-} catch (error) {
-  console.log('Event File does not exist yet');
-}
+watch(syncLocation, { delay: 5000 }, function (evt, name) {
+  if (evt == 'update') {
+    // delete file if it exists
+    try {
+      unlinkSync(eventFile);
+    } catch (error) {
+      console.log('Event File does not exist yet');
+    }
 
-// get races
-const races = getRaces(mdb, competition_no).sort((a, b) => raceCompare(a, b));
-const programs = getPrograms(mdb, competition_no);
-const lanes = getLanes(mdb, competition_no);
-const competitorsInComp = getCompetitorsInCompetition(mdb, competition_no);
-const competitors = getCompetitors(mdb);
-races.forEach((race) => {
-  // for every race print one line with the race info
-  const event = race.name.toString().replace(/[a-z]/gi, '');
-  const heat = letterToNumber(
-    race.name
-      .toString()
-      .match(/[a-zA-Z]/g)
-      .join('')
-  );
-  const programItem = programs.find((c) => c.id === race.programItemId);
+    // get races
+    const races = getRaces(mdb, competition_no).sort((a, b) =>
+      raceCompare(a, b)
+    );
+    const programs = getPrograms(mdb, competition_no);
+    const lanes = getLanes(mdb, competition_no);
+    const competitorsInComp = getCompetitorsInCompetition(mdb, competition_no);
+    const competitors = getCompetitors(mdb);
+    races.forEach((race) => {
+      // for every race print one line with the race info
+      const event = race.name.toString().replace(/[a-z]/gi, '');
+      const heat = letterToNumber(
+        race.name
+          .toString()
+          .match(/[a-zA-Z]/g)
+          .join('')
+      );
+      const programItem = programs.find((c) => c.id === race.programItemId);
 
-  appendFileSync(
-    eventFile,
-    `${event},${event},${heat},${race.name} ${programItem.group} ${programItem.length}m ${programItem.track}m\n`
-  );
-
-  // for every lane in the race print one line with the lane info
-  lanes
-    .filter((c) => c.raceId === race.id)
-    .sort((a, b) => a.startPosition - b.startPosition)
-    .forEach((lane) => {
-      const competitor = competitorsInComp.filter(
-        (c) => c.id === lane.skaterInCompetitionId
-      )[0];
-      const competitorInfo = competitors.filter(
-        (c) => c.id === competitor.competitorId
-      )[0];
       appendFileSync(
         eventFile,
-        `,${competitor.helmetId},${lane.startPosition},${competitorInfo.lastName},${competitorInfo.firstName},${competitor.affiliation},,${competitorInfo.id}\n`
+        `${event},${event},${heat},${race.name} ${programItem.group} ${programItem.length}m ${programItem.track}m\n`
       );
+
+      // for every lane in the race print one line with the lane info
+      lanes
+        .filter((c) => c.raceId === race.id)
+        .sort((a, b) => a.startPosition - b.startPosition)
+        .forEach((lane) => {
+          const competitor = competitorsInComp.filter(
+            (c) => c.id === lane.skaterInCompetitionId
+          )[0];
+          const competitorInfo = competitors.filter(
+            (c) => c.id === competitor.competitorId
+          )[0];
+          appendFileSync(
+            eventFile,
+            `,${competitor.helmetId},${lane.startPosition},${competitorInfo.lastName},${competitorInfo.firstName},${competitor.affiliation},,${competitorInfo.id}\n`
+          );
+        });
     });
+  }
+
+  if (evt == 'remove') {
+    // on delete
+  }
 });
 
 function letterToNumber(letter) {
