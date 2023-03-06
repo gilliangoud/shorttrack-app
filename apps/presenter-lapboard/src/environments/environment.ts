b16edcfd04f47ad59bbea1ef21df144da3b276c7
Gillian Goud <gilliangoud@hotmail.com>
2023-03-06 11:14:50 -0800

    remove old lapboard project

--- apps/presenter-lapboard/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// When building for production, this file is replaced with `environment.prod.ts`.
-
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://wuhnbxqejdishfovgtze.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1aG5ieHFlamRpc2hmb3ZndHplIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg4MDM5NzgsImV4cCI6MTk4NDM3OTk3OH0.mXqeA9zlNWTQbVYFlyYbYdCNR8iPSMu86vGZz3nFjso',
-};
