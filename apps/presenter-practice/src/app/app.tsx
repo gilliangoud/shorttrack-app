06e34f48cf0b967ab6cb5500aeb8ee7886585638
Gillian Goud <gilliangoud@hotmail.com>
2023-03-13 12:26:54 -0700

    update laptime display with simpeler logic, add animations

--- apps/presenter-practice/src/app/app.tsx
+++ apps/presenter-practice/src/app/app.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@shorttrack-app/st-app-db';
-import { useEffect, useState } from 'react';
+import { useState, useRef, useEffect } from 'react'
+import autoAnimate from '@formkit/auto-animate'
 import { atom, useAtom } from 'jotai';
 
 const projectURL = import.meta.env.VITE_PUBLIC_SUPABASE_URL || 'https://wuhnbxqejdishfovgtze.supabase.co';
@@ -9,44 +10,54 @@ const projectKey = import.meta.env.VITE_PUBLIC_SUPABASE_KEY || 'eyJhbGciOiJIUzI1
 const transpondersAtom = atom<
   Database['public']['Tables']['transponders']['Row'][]
 >([]);
-const passingsAtom = atom<Database['public']['Tables']['passings']['Row'][]>(
+// const passingsAtom = atom<Database['public']['Tables']['passings']['Row'][]>(
+//   []
+// );
+const lapTimesAtom = atom<Array<{ transponder: string; lap_times: string[]; lap_time: number }>>(
   []
 );
-const skatersAtom = atom<
-  Array<{
-    name: string;
-    lap_time: string;
-  }>
->((get) => {
-  const skaters: { name: string; lap_time: string }[] = [];
-  get(transpondersAtom).forEach(
-    (transponder: Database['public']['Tables']['transponders']['Row']) => {
-      // get all passings for this transponder sorted from newest to oldest
-      const passingsSorted = get(passingsAtom)
-        .filter((p) => p.tran_code === transponder.id || p.transponder === '9992' || p.tran_code === '9992')
-        .sort(
-          (a, b) =>
-            new Date(b.time || '').getTime() - new Date(a.time || '').getTime()
-        );
-      // console.log('for transponder ', transponder.id, ' passings are ', passingsSorted)
-      // calculate time between the two latest passings
-      const laptime =
-        passingsSorted.length > 1
-          ? new Date(passingsSorted[0].time || '').getTime() -
-            new Date(passingsSorted[1].time || '').getTime()
-          : 0;
-      skaters.push({
-        name: transponder.name || '',
-        lap_time: (parseInt(laptime.toString()) / 1000).toFixed(2),
-      });
-    }
-  );
-  return skaters;
-});
+// const skatersAtom = atom<
+//   Array<{
+//     name: string;
+//     lap_time: string;
+//   }>
+// >((get) => {
+//   const skaters: { name: string; lap_time: string }[] = [];
+//   get(transpondersAtom).forEach(
+//     (transponder: Database['public']['Tables']['transponders']['Row']) => {
+//       // get all passings for this transponder sorted from newest to oldest
+//       const passingsSorted = get(passingsAtom)
+//         .filter((p) => p.tran_code === transponder.id || p.transponder === '9992' || p.tran_code === '9992')
+//         .sort(
+//           (a, b) =>
+//             new Date(b.time || '').getTime() - new Date(a.time || '').getTime()
+//         );
+//       // console.log('for transponder ', transponder.id, ' passings are ', passingsSorted)
+//       // calculate time between the two latest passings
+//       const laptime =
+//         passingsSorted.length > 1
+//           ? new Date(passingsSorted[0].time || '').getTime() -
+//             new Date(passingsSorted[1].time || '').getTime()
+//           : 0;
+//       skaters.push({
+//         name: transponder.name || '',
+//         lap_time: (parseInt(laptime.toString()) / 1000).toFixed(2),
+//       });
+//     }
+//   );
+//   return skaters;
+// });
 
 export function App() {
   const [supabase] = useState(createClient<Database>(projectURL, projectKey));
+  const [lapTimes, setLapTimes] = useAtom(lapTimesAtom);
+  const parent = useRef(null)
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current)
+  }, [parent])
+
   const ws = new WebSocket("ws://192.168.1.2:8000");
+  // const ws = new WebSocket("ws://localhost:8000");
   ws.onopen = (event) => {
     console.log("WebSocket is open now.", event);
   };
@@ -66,15 +77,25 @@ export function App() {
       tran_code: string, //"TZ-02445",
       sport: number, //2
     } = JSON.parse(event.data);
-    console.log(JSON.stringify(json, null, 2))
+    // console.log(JSON.stringify(json, null, 2))
     try {
-      // if ((json.event = "data")) {
-      //   setBids(json.data.bids.slice(0, 5));
-      // }
       json.time = json.rtc_time;
-      setPassings([json as unknown as Database['public']['Tables']['passings']['Row'], ...passings]);
-    } catch (err) {
-      console.log(err);
+      // setPassings([json as unknown as Database['public']['Tables']['passings']['Row'], ...passings]);
+      // console.log('received update')
+      setLapTimes((prev) => {
+        let newValue = prev.find((x) => x.transponder === json.tran_code);
+        if (newValue === undefined) {
+          newValue = { transponder: json.tran_code, lap_times: [json.rtc_time], lap_time: 0 };
+        } else if (newValue.lap_times[0] === json.rtc_time) {
+          return prev;
+        } else if (newValue.lap_times.length > 0) {
+          newValue.lap_time = (new Date(json.rtc_time).getTime() - new Date(newValue.lap_times[0]).getTime());
+          newValue.lap_times = [json.rtc_time, newValue.lap_times[0]];
+        }
+        return [newValue, ...prev.filter((x) => x.transponder !== json.tran_code)];
+    })
+    } catch (error) {
+      console.error(error);
     }
   };
 
@@ -92,8 +113,8 @@ export function App() {
   // }>()
 
   const [transponders, setTransponders] = useAtom(transpondersAtom);
-  const [passings, setPassings] = useAtom(passingsAtom);
-  const [skaters, setSkaters] = useAtom(skatersAtom);
+  // const [passings, setPassings] = useAtom(passingsAtom);
+  // const [skaters, setSkaters] = useAtom(skatersAtom);
 
   // get the initial transponders, passings from the last hour and, subscribe to new passings
   useEffect(() => {
@@ -110,21 +131,21 @@ export function App() {
         }
       });
 
-    supabase
-      .from('passings')
-      .select('*')
-      .order('time', { ascending: false })
-      .limit(20)
-      .then((res) => {
-        if (res.error) {
-          console.error(res.error);
-          return;
-        }
-        if (res.data) {
-          setPassings(res.data);
-          console.log('set passings', res.data, passings);
-        }
-      });
+    // supabase
+    //   .from('passings')
+    //   .select('*')
+    //   .order('time', { ascending: false })
+    //   .limit(20)
+    //   .then((res) => {
+    //     if (res.error) {
+    //       console.error(res.error);
+    //       return;
+    //     }
+    //     if (res.data) {
+    //       setPassings(res.data);
+    //       console.log('set passings', res.data, passings);
+    //     }
+    //   });
 
     const subscription = supabase
       .channel('practice')
@@ -137,14 +158,14 @@ export function App() {
         },
         (payload) => {
           // if (passings.findIndex((p) => p.time === payload.new.time) > -1) return;
-          console.log('new passing', payload.new);
-          console.log('passings before', passings);
-          setPassings((prev) => [
-            payload.new as Database['public']['Tables']['passings']['Row'],
-            ...prev,
-          ]);
-          console.log('skaters', skaters);
-          console.log('passings after', passings);
+          // console.log('new passing', payload.new);
+          // console.log('passings before', passings);
+          // setPassings((prev) => [
+          //   payload.new as Database['public']['Tables']['passings']['Row'],
+          //   ...prev,
+          // ]);
+          // console.log('skaters', skaters);
+          // console.log('passings after', passings);
         }
       )
       .subscribe();
@@ -155,21 +176,20 @@ export function App() {
   }, []);
 
   return (
-      <ul className="h-screen bg-gray-900 space-y-2 overflow-auto flex flex-wrap justify-evenly">
-        {skaters
-          .filter((s) => s.lap_time !== '0.00')
+      <ul className="h-screen bg-gray-900 space-y-2 overflow-auto flex flex-wrap justify-evenly" ref={parent}>
+        {lapTimes
           .map((item) => {
             return (
               <li
-                key={item.name}
+                key={item.transponder}
                 className="w-3/12 min-w-fit flex-grow-0 overflow-hidden m-1 max-h-56 bg-slate-800 px-4 py-2 shadow sm:rounded-md"
               >
                 <div className="flex space-x-1 overflow-clip -my-8">
                   <p className="flex-1 text-clip text-5xl object-fit object-scale-down self-center font-medium tracking-tight text-gray-300">
-                    {item.name}
+                    {transponders.find((x) => x.id === item.transponder) ? transponders.find((x) => x.id === item.transponder)?.name : item.transponder}
                   </p>
                   <p className="self-center text-[11em] font-bold tracking-tight text-white text-right">
-                    {item.lap_time}
+                    {msToTime(item.lap_time)}
                   </p>
                 </div>
               </li>
@@ -180,6 +200,13 @@ export function App() {
 }
 
 export default App;
+
+// convert milliseconds to time string with format ss:ms limited to 2 decimals
+export function msToTime(duration: number) {
+  return `${(duration / 1000).toFixed(2).toString()}`;
+}
+
+
 
 if (import.meta.vitest) {
   // add tests related to your file here
